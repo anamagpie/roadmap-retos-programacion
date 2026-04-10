76d5131f06441367f2eb34969bf27e2b39001d25
Ana <102044859+anamagpie@users.noreply.github.com>
2026-04-10 09:02:04 +0000

    #00 - Python

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/anamagpie.py
@@ -0,0 +1,39 @@
+'''
+ EJERCICIO:
+ - Crea un comentario en el código y coloca la URL del sitio web oficial del
+   lenguaje de programación que has seleccionado.
+ - Representa las diferentes sintaxis que existen de crear comentarios
+   en el lenguaje (en una línea, varias...).
+ - Crea una variable (y una constante si el lenguaje lo soporta).
+ - Crea variables representando todos los tipos de datos primitivos
+   del lenguaje (cadenas de texto, enteros, booleanos...).
+ - Imprime por terminal el texto: "¡Hola, [y el nombre de tu lenguaje]!
+'''
+
+#https//python.org
+
+#Comentario en una linea
+
+"""
+Comentario en varias lineas.
+Igual que en el enunciado.
+"""
+'''
+También se puede
+hacer así
+'''
+
+avariable = "Una variable"
+avariable = "Un nuevo valor para la misma variable"
+
+#Python no tiene constantes /// Se puede comentar: MY_CONSTANT = "Mi constante" en mayúsculas para avisar
+
+my_int = 28
+my_float = 90.5
+my_bool = True
+my_bool = False
+my_string = "Una cadena de texto" #Se pueden usar comillas simples
+my_string2 = 'Una cadena de texto con comillas simples'
+
+
+print(*"Hola, Python!"*)
