65aa6c5d7601f72d58d868e1f4adf34222cdb30b
Ana <102044859+anamagpie@users.noreply.github.com>
2026-04-17 07:14:54 +0000

    #23 - Python

--- Roadmap/23 - SINGLETON/python/anamagpie.py
+++ Roadmap/23 - SINGLETON/python/anamagpie.py
@@ -35,42 +35,50 @@ print(prueba1 is prueba2)
 
 class UserSession:
 
+# Instancia
     _instance = None
 
+# Datos del usuario
     id: int = None
     username: str = None
     name: str = None
     email: str = None
 
+#Igual que en primer ejercicio
     def __new__(cls):
         if not cls._instance:
             cls._instance = super(UserSession, cls).__new__(cls)
         return cls._instance
 
+# Permite asignar un usuario
     def set_user(self, id, username, name, email):
         self.id = id
         self.username = username
         self.name = name
         self.email = email
 
+# Recupera los datos del usuario
     def get_user(self):
         return f"{self.id}, {self.username}, {self.name}, {self.email}"
 
+# Borra los datos de la sesión
     def clear_user(self):
         self.id = None
         self.username = None
         self.name = None
         self.email = None
 
-
+# Asigna al usuario "inventpers0n"
 session1 = UserSession()
 print(session1.get_user())
 session1.set_user(135455, "inventpers0n", "Invent Person", "inv_pers0n@inve.com")
 print(session1.get_user())
 
+# Recupera los datos de "inventpers0n"
 session2 = UserSession()
 print(session2.get_user())
 
+# Borra los datos de la sesión
 session3 = UserSession()
 session3.clear_user()
 print(session3.get_user())
